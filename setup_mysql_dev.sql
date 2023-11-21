e8488ea4dace700170136abf35c1da410d85cdec
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-21 19:06:31 +0300

    comment added each line

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,13 @@
 --  prepares a MySQL server for the project. 
---  database hbnb_dev_db, new user, priveleges. 
 
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the specified database to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
