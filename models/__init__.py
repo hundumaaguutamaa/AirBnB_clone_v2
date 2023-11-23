33ec256eb0be29ccb7dd743f99b6b5e6be8c62cb
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-22 20:59:01 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+""" Module instantiates an instance of the Storage will be used. """
 
-""" This module instantiates an object of class FileStorage. """
+from os import getenv
 
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-import os
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
+    
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
